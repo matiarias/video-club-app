776c19d896b4eba6d2d2857ce30d1da15e858061
Matias Arias <matt.arias182@gmail.com>
2022-11-10 07:01:43 -0300

    se cambio a color indigo los elementos del navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
 const NavBar = () => {
   return (
     <div className="w-full flex justify-between items-center px-6 py-4 z-[100] absolute top-0 left-0">
-      <h1 className="text-2xl md:text-4xl font-bold text-red-700 cursor-pointer">
+      <h1 className="text-2xl md:text-4xl font-bold text-indigo-600 cursor-pointer">
         VideoClub
       </h1>
       <div>
         <button className="text-white pr-4 mr-4">Sign In</button>
-        <button className="bg-red-700 text-white px-6 py-2 rounded-md cursor-pointer">
+        <button className="bg-indigo-600 text-white px-6 py-2 rounded-md cursor-pointer">
           Sign Up
         </button>
       </div>
