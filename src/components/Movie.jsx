84b95a0215d560c02a400ed1503b2f008129624f
Matias Arias <matt.arias182@gmail.com>
2022-11-13 00:59:37 -0300

    sese agrego mas width al componente movies

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -6,7 +6,7 @@ const Movie = ({ item }) => {
 
   return (
     <>
-      <div className="w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
+      <div className="w-[200px] md:w-[250px] lg:w-[300px] inline-block cursor-pointer relative p-2">
         <img
           className="w-full h-auto block"
           src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
