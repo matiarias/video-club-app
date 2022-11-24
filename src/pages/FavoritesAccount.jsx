b60e85eb6691f482f608b3f9a3bf9ca9eec8c033
Matias Arias <matt.arias182@gmail.com>
2022-11-24 02:27:24 -0300

    se agrego background image al componente account

--- src/pages/FavoritesAccount.jsx
+++ src/pages/FavoritesAccount.jsx
@@ -1,7 +1,17 @@
+import MoviesCollage from "../assets/movies-collage.jpg";
+
 const FavoritesAccount = () => {
   return (
-    <div className="h-screen w-screen">
-      <h1 className="text-white text-3xl">Your favourites Movies</h1>
+    <div className="relative min-h-screen w-screen">
+      <div className="h-[60vh] w-full">
+        <img
+          className="h-full w-full object-cover object-center"
+          src={MoviesCollage}
+          alt="Movies Collage"
+        />
+      </div>
+      <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-r from-black/80"></div>
+      <div className="absolute top-0 left-0 h-full w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-12"></div>
     </div>
   );
 };
